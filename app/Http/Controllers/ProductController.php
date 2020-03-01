abdab6beefa9f5c9fa6187d4103ac739044b915f
Alex Esaki <apachecordovax@gmail.com>
2020-03-01 16:20:52 +0100

    Afficher les produits

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Product;
+
+class ProductController extends Controller
+{
+    public function index()
+    {
+        $products = Product::inRandomOrder()->take(6)->get();
+
+        return view('products.index')->with('products', $products);
+    }
+
+    public function show($slug)
+    {
+        $product = Product::where('slug', $slug)->firstOrFail();
+
+        return view('products.show')->with('product', $product);
+    }
+}
