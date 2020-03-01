e80445dff037a91844f7b270ab66aa38271b40a3
Alex Esaki <apachecordovax@gmail.com>
2020-03-01 21:47:35 +0100

    afficher le panier

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -15,7 +15,7 @@ class CartController extends Controller
      */
     public function index()
     {
-        //
+        return view('cart.index');
     }
 
     /**
@@ -36,7 +36,17 @@ class CartController extends Controller
      */
     public function store(Request $request)
     {
-        Cart::add($request->id, $request->title, 1, $request->price)
+        $duplicate = Cart::search(function ($cartItem, $rowId) use ($request) {
+            return $cartItem->id == $request->product_id;
+        });
+
+        if ($duplicate->isNotEmpty()) {
+            return redirect()->route('products.index')->with('success', 'Le produit a déjà été ajouté.');
+        }
+
+        $product = Product::find($request->product_id);
+
+        Cart::add($product->id, $product->title, 1, $product->price)
             ->associate(Product::class);
 
         return redirect()->route('products.index')->with('success', 'Le produit a bien été ajouté.');
@@ -82,8 +92,10 @@ class CartController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function destroy($id)
+    public function destroy($rowId)
     {
-        //
+        Cart::remove($rowId);
+
+        return back()->with('success', 'Le produit a été supprimé.');
     }
 }
--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -14,6 +14,9 @@
     <!-- Bootstrap core CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
 
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+
     <!-- Favicons -->
     <link rel="apple-touch-icon" href="/docs/4.4/assets/img/favicons/apple-touch-icon.png" sizes="180x180">
     <link rel="icon" href="/docs/4.4/assets/img/favicons/favicon-32x32.png" sizes="32x32" type="image/png">
@@ -158,7 +161,7 @@ h1, h2, h3, h4, h5, h6 {
   <header class="blog-header py-3">
     <div class="row flex-nowrap justify-content-between align-items-center">
       <div class="col-4 pt-1">
-        <a class="text-muted" href="#">Panier <span class="badge badge-pill badge-dark">{{ Cart::count() }}</span></a>
+      <a class="text-muted" href="{{ route('cart.index') }}">Panier <span class="badge badge-pill badge-dark"> {{ Cart::count()  }}</span></a>
       </div>
       <div class="col-4 text-center">
         <a class="blog-header-logo text-dark" href="{{ route('products.index') }}">Larashop</a>
@@ -195,13 +198,15 @@ h1, h2, h3, h4, h5, h6 {
     </div>
   @endif
 
-  <div class="jumbotron p-4 p-md-5 text-white rounded bg-dark">
+  <!--
+    <div class="jumbotron p-4 p-md-5 text-white rounded bg-dark">
     <div class="col-md-6 px-0">
       <h1 class="display-4 font-italic">Title of a longer featured blog post</h1>
       <p class="lead my-3">Multiple lines of text that form the lede, informing new readers quickly and efficiently about what’s most interesting in this post’s contents.</p>
       <p class="lead mb-0"><a href="#" class="text-white font-weight-bold">Continue reading...</a></p>
     </div>
   </div>
+  -->
 
   <div class="row mb-2">
     @yield('content')
